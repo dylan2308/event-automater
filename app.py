999279a2f955b17b637f760ff7af6683149437a1
jordon0802 <jordon.chang.jj@gmail.com>
2024-01-07 00:08:59 +0800

    remove check.py after 'merging' with app.py

--- app.py
+++ app.py
@@ -35,17 +35,77 @@ def main():
     with open("token.json", "w") as token:
       token.write(creds.to_json())
 
+  title = input("Event Title: ")
+          
+  startYear, startMth, startDay, startHr, startMin, startS = [0, 0, 0, 0, 0, 0]
+  endYear, endMth, endDay, endHr, endMin, endS = [0, 0, 0, 0, 0, 0]
+
+  # Check valid year
+  while startYear < 2024 or startYear > 2100:
+      while True:
+          try:
+              startYear = int(input("Enter a valid start year: "))
+              break
+          except:
+              continue
+  # Check valid month i.e. between 1-12
+  while startMth not in range(1, 13):
+      while True:
+          try:
+              startMth = int(input("Enter a valid start month: "))
+              break
+          except:
+              continue
+  # Check months with 31 days
+  if startMth in [1, 3, 5, 7, 8, 10, 12]:
+      while startDay not in range(1, 32):
+          while True:
+              try:
+                  startDay = int(input("Enter a valid start day: "))
+                  break
+              except:
+                  continue
+  # Check months with 30 days
+  elif startMth in [4, 6, 9, 11]:
+      while startDay not in range(1, 31):
+          while True:
+              try:
+                  startDay = int(input("Enter a valid start day: "))
+                  break
+              except:
+                  continue
+  # Check February
+  elif startMth == 2:
+      # Leap years are divisible by 4
+      if startYear % 4 == 0:
+          while startDay not in range(1, 30):
+              while True:
+                  try:
+                      startDay = int(input("Enter a valid start day: "))
+                      break
+                  except:
+                      continue
+      else:
+          while startDay not in range(1, 29):
+              while True:
+                  try:
+                      startDay = int(input("Enter a valid start day: "))
+                      break
+                  except:
+                      continue
+  
+  startDate = str(startYear) + "-" + str(startMth) + "-" + str(startDay)
 
   event = {
-    "summary": "testEvent",
+    "summary": f"{title}",
 
     "start": {
-      "dateTime": "2024-01-10T10:00:00+08:00",
+      "dateTime": f"{startDate}T10:00:00",
       "timeZone": "Asia/Singapore"
     },
 
     "end": {
-      "dateTime": "2024-01-11T10:00:00+08:00",
+      "dateTime": "2024-01-11T10:00:00",
       "timeZone": "Asia/Singapore"
     }
   }
